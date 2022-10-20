b270eb08efec0be8b8bc97ccd2c03bdd38918d78
floz <florian.zumbrunn@gmail.com>
2022-10-20 14:37:29 +0200

    part 2

--- 02_physical-material/index.js
+++ 02_physical-material/index.js
@@ -2,23 +2,23 @@ import * as THREE from '../__common/libs/three.module';
 
 import Engine from '../__common/engine'
 
+const engine = new Engine()
+
+// Setup
 let scene = null
 let camera = null
 
-// Pour nous faciliter la vie : s'occupe du resize de la window auto
-// et de creer notre THREE.WebGLRenderer
-const engine = new Engine()
+// Contents
+let mesh = null
 
 function setup() {
-	// On a besoin d'une scene
 	scene = new THREE.Scene()
-	
-	// Et d'une camera
+
 	camera = new THREE.PerspectiveCamera( 50, engine.width / engine.height, .1, 1000 )
 	camera.position.z = 10
 }
 
-function setupScene() {
+function createScene() {
 	const ambient = new THREE.AmbientLight( 0x202020 )
 	scene.add( ambient )
 
@@ -28,7 +28,7 @@ function setupScene() {
 	lightBehind.position.z = -5
 	scene.add( lightBehind )
 
-	const lightFront = new THREE.PointLight( 0x0000ff, .5 )
+	const lightFront = new THREE.PointLight( 0xffffff, 1 )
 	lightFront.position.x = -5
 	lightFront.position.y = -5
 	lightFront.position.z = 5
@@ -38,25 +38,23 @@ function setupScene() {
 function createMesh() {
 	const geometry = new THREE.SphereGeometry( 1, 32, 32 )
 	const material = new THREE.MeshPhysicalMaterial( {
-		color: 0xff00ff
+		color: 0xff00ff,
 	} )
 
-	const mesh = new THREE.Mesh( geometry, material )
+	mesh = new THREE.Mesh( geometry, material )
 	scene.add( mesh )
 }
 
 setup()
-setupScene()
+
+createScene()
 createMesh()
 
 function render() {
-	// Update ce qu'il faut
-	
-	// Render la scene
 	engine.renderer.render( scene, camera )
 }
 
-// Notre frame loop
+// On frame loop
 function onFrame() {
 	requestAnimationFrame( onFrame )
 
